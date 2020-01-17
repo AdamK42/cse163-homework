555333cda5bc551aa6ecf6844d0ee2b4586b69f5
Adam Klingler <aoklingler@gmail.com>
2020-01-17 15:34:19 -0800

    hw2 files downloaded

---
+++ hw2/hw2_test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Don't worry about this import syntax, we will talk about it later
+# You can call the method using
+#    assert_equals(expected, received)
+#    parse(file)
+from cse163_utils import assert_equals, parse
+
+import hw2_manual
+import hw2_pandas
+
+# This file is left blank for you to fill in with your tests!
