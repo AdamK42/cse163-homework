bb1c3683751e6ba9f405333e532fe229e386ff26
Adam Klingler <aoklingler@gmail.com>
2020-01-30 18:03:43 -0800

    File started

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,6 +1,13 @@
-import cse163_utils  # noqa: F401
-# This is a hacky workaround to an unfortunate bug on macs that causes an
-# issue with seaborn, the graphing library we want you to use on this
-# assignment.  You can just ignore the above line or delete it if you are
-# not using a mac!
-# Add your imports and code here!
+# Name: Adam Klingler
+# Assignment: Homework 3
+# Section: AB
+# Description:
+
+# Imports here
+import cse163_utils
+import pandas as pd
+import seaborn as sns
+
+# Global constants here
+
+# Functions here
