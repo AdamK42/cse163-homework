853de687b6b7f5a180aa08e172005c41c5b851df
Adam Klingler <aoklingler@gmail.com>
2020-01-30 23:02:06 -0800

    Function stubs written

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -8,6 +8,69 @@ import cse163_utils
 import pandas as pd
 import seaborn as sns
 
-# Global constants here
-
 # Functions here
+# Part 0
+
+
+def completions_between_years(df, begin, end, sex):
+    '''
+    '''
+    pass
+
+
+def compare_bachelors_1980(df):
+    '''
+    '''
+    pass
+
+
+def top_2_2000s(df):
+    '''
+    '''
+    pass
+
+
+def percent_change_bachelors_2000s(df):
+    '''
+    '''
+    pass
+
+
+# Part 1
+
+
+def line_plot_bachelors(df):
+    '''
+    '''
+    pass
+
+
+def bar_chart_high_school(df):
+    '''
+    '''
+    pass
+
+
+def plot_hispanic_min_degree(df):
+    '''
+    '''
+    pass
+
+
+# part 2
+
+
+def fit_and_predict_degrees(df):
+    '''
+    '''
+    pass
+
+
+def main():
+    '''
+    '''
+    pass
+
+
+if __name__ == '__main__':
+    main()
