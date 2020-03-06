8507c3e6272daa4d6f7e7904234853db27ad3113
Adam Klingler <aoklingler@gmail.com>
2020-03-05 18:58:08 -0800

    Problem 3 implemented, all problems fixed

--- hw6/hw6_main.py
+++ hw6/hw6_main.py
@@ -1,13 +1,10 @@
 # Name: Adam Klingler
 # Section: AB
-# Description:
+# Description: This file contains functions that manipulate images in various
+#              ways.
 
 # imports here
 import numpy as np
-import matplotlib.pyplot as plt
-import imageio
-
-# constants here
 
 # functions here
 
@@ -17,7 +14,7 @@ def invert_colors(img):
     This function takes in a color image and returns an image with inverted
     color.
     '''
-    height, width, depth = img.shape()
+    height, width, depth = img.shape
     inverter = np.ones((height, width, depth)) * 255  # max value of a pixel
 
     return inverter - img
@@ -39,12 +36,32 @@ def blur(img, patch_size):
     for i in range(result_h):
         for j in range(result_w):
             window = img[i:i+patch_size, j:j+patch_size]
-            result[i, j] = np.multiply(window, kernel)
+            result[i, j] = np.sum(np.multiply(window, kernel))
 
-    return result
+    return result.astype(np.uint8)
 
 
 def template_match(img, template):
     '''
+    This function takes in a black and white image and a template and finds the
+    cross-corelation of the template and image, returning those values.
     '''
-    pass
+    height, width = img.shape
+    t_height, t_width = template.shape
+    result_h = height - t_height + 1
+    result_w = width - t_width + 1
+
+    result = np.zeros((result_h, result_w))
+
+    template_mean = np.mean(template)
+    kernel = template - template_mean
+
+    for i in range(result_h):
+        for j in range(result_w):
+            window = img[i:i+t_height, j:j+t_width]
+            window_mean = np.mean(window)
+            fixed_window = window - window_mean
+
+            result[i, j] = np.sum(np.multiply(fixed_window, kernel))
+
+    return result
